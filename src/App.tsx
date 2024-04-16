c6e443a149aef84a31784ecf09d0ba4d5d5c8e2c
Ryan Richard <mrrarichard@gmail.com>
2024-04-16 19:59:27 +0200

    fixed router bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import HomePage from './components/HomePage/HomePage';
